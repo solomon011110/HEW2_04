849113d617692c7f433c29749c23209e985fff55
ShiningSpring <harutoto1101@gmail.com>
2024-07-11 14:20:17 +0900

    Create main.py

---
+++ hello/main.py
@@ -0,0 +1,11 @@
+from flask import Flask
+from waitress import serve
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return 'Hello, World!'
+
+if __name__ == '__main__':
+    serve(app, host='0.0.0.0', port=5000)
