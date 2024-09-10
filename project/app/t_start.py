95622310a77a55fc976d6dfeea38d37f49a0aae0
solomon011110 <ths31065@ths.hal.ac.jp>
2024-09-10 11:22:08 +0900

    db作成修正2

--- project/app/t_start.py
+++ project/app/t_start.py
@@ -7,28 +7,22 @@ conn = sqlite3.connect(dbname)
 cur = conn.cursor()
 
 cur.execute("""\
-CREATE TABLE IF NOT EXISTS T_users (
-    F_id TEXT PRIMARY KEY,
-    F_username TEXT NOT NULL,
-    F_email TEXT NOT NULL
+CREATE TABLE T_users (\
+    F_id TEXT PRIMARY KEY,\
+    F_username TEXT NOT NULL,\
+    F_email TEXT NOT NULL\
 );\
 """)
 conn.commit()
 
 cur.execute("""\
-    CREATE TABLE IF NOT EXISTS T_item (
-    F_id TEXT PRIMARY KEY,
-    F_name TEXT NOT NULL,
-    F_description TEXT,
-    F_imgPass TEXT
+    CREATE TABLE T_item (\
+    F_id TEXT PRIMARY KEY,\
+    F_name TEXT NOT NULL,\
+    F_description TEXT,\
+    F_imgPass TEXT\
 );\
 """)
 conn.commit()
-
-cur.execute("""\
-  INSERT INTO T_item
-  (F_id, F_name, F_description, F_imgPass)
-  VALUES ('00001', 'テストプロジェクト', '00001.txt', 'apple.jpg')\
-""")
 
 conn.close()
