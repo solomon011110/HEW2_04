9721c1600d5c60ee5ed0a8c3d66716db1861047c
ShiningSpring <harutoto1101@gmail.com>
2024-11-29 12:15:40 +0900

    ggez

--- project/app/routes.py
+++ project/app/routes.py
@@ -14,13 +14,14 @@ admin_credentials = {
 }
 
 
-
 def generate_verification_code():
     return str(1)
+
 
 @bp.route('/')
 def index():
     return render_template('home.html')
+
 
 @bp.route('/goods/<id>')
 def goods(id):
@@ -29,9 +30,7 @@ def goods(id):
     return render_template('goods.html', product=product, image_url=image_url)
 
 
-    
-
-@bp.route('/login', methods=['GET','POST'])
+@bp.route('/login', methods=['GET', 'POST'])
 def login():
     if request.method == "POST":
         email = request.form.get('email')
@@ -40,11 +39,12 @@ def login():
         user = User.query.filter_by(email=email).first()
         if user and check_password_hash(user.password, password):
             login_user(user)
-            return redirect('/profile')  
+            return redirect('/profile')
         else:
             flash('ユーザー名またはパスワードが間違っています。')
             return redirect('/login')
     return render_template('login.html')
+
 
 @bp.route('/logout')
 @login_required
@@ -52,35 +52,39 @@ def logout():
     logout_user()
     return redirect('/')
 
-@bp.route('/register', methods=['GET','POST'])
+
+@bp.route('/register', methods=['GET', 'POST'])
 def register():
     if request.method == 'POST':
         email = request.form['email']
-        password = generate_password_hash(request.form['password'], method='pbkdf2:sha256')
+        password = generate_password_hash(
+            request.form['password'], method='pbkdf2:sha256')
         # 認証コード生成
         verification_code = generate_verification_code()
-        
+
         # セッションに認証コードを保存
         session['verification_code'] = verification_code
         session['email'] = email
         session['password'] = password
-        
+
         # 認証コードをメールで送信
-        msg = Message('Your Verification Code', sender='hewgroup040@gmail.com', recipients=[email])
+        msg = Message('Your Verification Code',
+                      sender='hewgroup040@gmail.com', recipients=[email])
         msg.body = f'Your verification code is: {verification_code}'
         mail.send(msg)
-        
+
         flash('認証コードがメールに送信されました。')
         return redirect(url_for('main.verify'))
-    
+
     return render_template('register.html')
 
-@bp.route('/verify', methods=['GET','POST'])
+
+@bp.route('/verify', methods=['GET', 'POST'])
 def verify():
     if request.method == 'POST':
         input_code = request.form['verification_code']
         stored_code = session.get('verification_code')
-        
+
         if input_code == stored_code:
             flash('認証に成功しました！登録が完了しました。')
             id = session.get('id')
@@ -93,21 +97,24 @@ def verify():
             return redirect("login")
         else:
             flash('認証コードが正しくありません。再試行してください。')
-    
+
     return render_template('verify.html')
+
 
 @bp.route('/profile')
 @login_required
 def profile():
     return render_template('profile.html', email=current_user.email)
-#一般アカウント----------------------------------------------
+# 一般アカウント----------------------------------------------
 
-@bp.route('/faq', methods=['GET','POST'])
+
+@bp.route('/faq', methods=['GET', 'POST'])
 def faq():
-# 要DB追加
-    return render_template('faq.html', faqs=faqs) 
+    # 要DB追加
+    return render_template('faq.html', faqs=faqs)
 
-@bp.route('/contact', methods=['GET','POST'])
+
+@bp.route('/contact', methods=['GET', 'POST'])
 def contact():
     if request.method == 'POST':
         email = request.form['email']
@@ -122,11 +129,10 @@ def contact():
         return redirect(url_for('home.html'))
     return render_template('contact.html')
 
-@bp.route('/main_market',  methods=['GET','POST'])
+
+@bp.route('/main_market',  methods=['GET', 'POST'])
 def main_market():
     return render_template('main_market')
-
-
 
 
 @bp.route('/project_info')
@@ -134,10 +140,12 @@ def main_market():
 def project_info():
     return render_template('project_info.html')
 
+
 @bp.route('/create_project')
 @login_required
 def create_project():
     return render_template('create_project.html')
+
 
 @bp.route('/project_list')
 @login_required
@@ -156,36 +164,29 @@ def purchase():
     return render_template('purchase.html')
 
 
-
 @bp.route('/purchasecon')
 @login_required
 def purchasecon():
     return render_template('purchasecon.html')
 
 
-
-
-
-
-
-
-
-#admin--------------------------------------------------------------
+# admin--------------------------------------------------------------
 def admin_required(f):
     @wraps(f)
     def decorated_function(*args, **kwargs):
         if not session.get('admin_logged_in'):
             flash('Please log in as an admin to access this page.')
-            return redirect(url_for('login'))
+            return redirect(url_for('main.admin_login'))
         return f(*args, **kwargs)
     return decorated_function
+
 
 @bp.route('/admin_login', methods=['GET', 'POST'])
 def admin_login():
     if request.method == 'POST':
         username = request.form['username']
         password = request.form['password']
-        
+
         # 認証チェック
         if username == admin_credentials['username'] and password == admin_credentials['password']:
             session['admin_logged_in'] = True  # セッションにログイン状態を保持
@@ -196,11 +197,11 @@ def admin_login():
 
     return render_template('admin_login.html')
 
+
 @bp.route('/admin_logout', methods=['GET', 'POST'])
 @admin_required
 def admin_logout():
-    session['admin_logged_in'] = False  
-
+    session['admin_logged_in'] = False
 
 
 @bp.route('/admin_dashboard')
@@ -213,7 +214,6 @@ def admin_dashboard():
     return render_template('admin_dashboard.html', name=current_user.email, users=users, inventories=inventories, product=product)
 
 
-
 @bp.route('/delete_user/<int:user_id>', methods=['POST'])
 @admin_required
 def delete_user(user_id):
@@ -221,12 +221,11 @@ def delete_user(user_id):
         return "アクセス権がありません", 403
 
     user = User.query.get(user_id)
-    if user and user_id != 3:
+    if user and user_id != 1:
         db.session.delete(user)
         db.session.commit()
         return redirect(url_for('main.admin_dashboard'))
     return "ユーザーが見つかりません", 404
-
 
 
 # 商品追加
@@ -243,7 +242,6 @@ def add_product():
     db.session.commit()
     flash("商品が追加されました。")
     return redirect(url_for('main.admin_dashboard'))
-
 
 
 # 商品更新
@@ -263,14 +261,13 @@ def update_product(product_id):
     return redirect(url_for('main.admin_dashboard'))
 
 
-
 # 在庫追加
 @bp.route('/add_inventory', methods=['POST'])
 @admin_required
 def add_inventory():
     product_id = request.form.get('product_id')
     quantity = request.form.get('quantity')
-    
+
     # 商品が存在するか確認
     product = Product.query.get(product_id)
     if not product:
@@ -281,12 +278,11 @@ def add_inventory():
     new_inventory = Inventory(
         product_id=product_id,
         quantity=quantity,)
-    
+
     db.session.add(new_inventory)
     db.session.commit()
     flash("在庫が追加されました。")
     return redirect(url_for('main.admin_dashboard'))
-
 
 
 # 在庫削除
@@ -304,7 +300,6 @@ def delete_inventory(inventory_id):
     return redirect(url_for('main.admin_dashboard'))
 
 
-
 # 在庫更新
 @bp.route('/update_inventory/<int:inventory_id>', methods=['POST'])
 @admin_required
@@ -320,4 +315,4 @@ def update_inventory(inventory_id):
     db.session.commit()
     flash("在庫が更新されました。")
     return redirect(url_for('main.admin_dashboard'))
-#--------------------------------------------------------------admin
+# --------------------------------------------------------------admin
